29c01a796f2990644a6e9b81c6910c422f3beb70
julieherrick <julieann4536@gmail.com>
2019-06-24 09:07:39 -0500

    Update filters.py

--- filters.py
+++ filters.py
@@ -10,3 +10,15 @@ def show_img(im):
 def save_img(im, filename):
   im.save(filename, "jpeg")
   show_img(im)
+  
+def obamicon(im):
+   # Load the pixel data from im.
+    
+   # Create a list to hold the new image pixel data.
+  
+   # Define color constants to use for recoloring.
+    
+   # Process the pixels in the image.
+      # Pixel intensity = R value + G value + B value
+    
+    # Save the filtered pixels as a new image
